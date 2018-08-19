86b9602d77542125f4c911d041655703041c0956
am969 <am969@cam.ac.uk>
2018-08-19 14:43:30 +0100

    Added old preprocessing script

--- preprocessing/step_preprocess_data.py
+++ preprocessing/step_preprocess_data.py
@@ -46,51 +46,61 @@ def main(argv=None):
 
     os.chdir(args.destination_dir)
 
+    #Load responses - check out load_text files...
     responses, _ = load_text(args.input_data_path, args.input_wlist_path)
-    prompts,   _ = load_text(args.input_prompt_path, args.input_wlist_path)
+    prompts, _ = load_text(args.input_prompt_path, args.input_wlist_path)
 
+
+    #Load up the prompts file
     with open(args.input_prompt_path, 'r') as file:
         topics = [line.replace('\n', '') for line in file.readlines()]
 
+    # Get unique set of topics and topic counts (and sort tem)
     unique_topics, topic_counts = np.unique(topics, return_counts=True)
-    topics=unique_topics[np.flip(np.argsort(topic_counts),0)]
-    topic_counts=np.flip(np.sort(topic_counts),0)
+    topics = unique_topics[np.flip(np.argsort(topic_counts), 0)]
+    topic_counts = np.flip(np.sort(topic_counts), 0)
 
-    topic_dict={}
+    #Create dictionary for topics mapping sentence to topic id
+    # Also create file of sorted topics and unigrams file
+    # Unigram file later used for training
+    topic_dict = {}
     with open('unigrams.txt', 'w') as ufile:
         with open('sorted_topics.txt', 'w') as tfile:
             for i, topic, count in zip(xrange(topics.shape[0]), topics, topic_counts):
-                topic_dict[topic]=i
+                topic_dict[topic] = i
                 ufile.write(str(i) + ',' + str(int(count)) + '\n')
-                tfile.write(topic+'\n')
+                tfile.write(topic + '\n')
 
+    # Load up the speakers and speakers
     grades = np.loadtxt(args.input_grade_path)
     with open(args.input_spkr_path, 'r') as file:
         speakers = [line.replace('\n', '') for line in file.readlines()]
 
+    # Create a list of topic IDs for every response
     with open(args.input_prompt_path, 'r') as file:
         q_ids = [topic_dict[line.replace('\n', '')] for line in file.readlines()]
 
     ### Split data into train and validation  data sets
-    size=len(responses)
-    valid_size=int(size*args.valid_fraction)
-    train_size=size-valid_size
+    size = len(responses)
+    valid_size = int(size * args.valid_fraction)
+    train_size = size - valid_size
 
     print 'Total dataset size', size, 'Train dataset size', train_size, 'Valid dataset size', valid_size
 
-    trn_responses=responses[valid_size:]
-    trn_prompts=prompts[valid_size:]
-    trn_q_ids=q_ids[valid_size:]
-    trn_speakers=speakers[valid_size:]
-    trn_grades=grades[valid_size:]
+    trn_responses = responses[valid_size:]
+    trn_prompts = prompts[valid_size:]
+    trn_q_ids = q_ids[valid_size:]
+    trn_speakers = speakers[valid_size:]
+    trn_grades = grades[valid_size:]
 
-    valid_responses=responses[:valid_size]
-    valid_prompts=prompts[:valid_size]
-    valid_q_ids=q_ids[:valid_size]
-    valid_speakers=speakers[:valid_size]
-    valid_grades=grades[:valid_size]
+    valid_responses = responses[:valid_size]
+    valid_prompts = prompts[:valid_size]
+    valid_q_ids = q_ids[:valid_size]
+    valid_speakers = speakers[:valid_size]
+    valid_grades = grades[:valid_size]
 
-    #Create the training TF Record file
+    # Create the training TF Record file
+    ### Maybe want to split into multiple files???
     filename = 'relevance.train.tfrecords'
     print 'Writing', filename
     writer = tf.python_io.TFRecordWriter(filename)
@@ -100,9 +110,9 @@ def main(argv=None):
                 'targets': tfrecord_utils.float_feature([1.0]),
                 'grade': tfrecord_utils.float_feature([grd]),
                 'spkr': tfrecord_utils.bytes_feature([spkr]),
-                'q_id': tfrecord_utils.int64_feature([q_id])#,
-                #'section': tfrecord_utils.int64_feature(section),
-             }),
+                'q_id': tfrecord_utils.int64_feature([q_id])  # ,
+                # 'section': tfrecord_utils.int64_feature(section),
+            }),
             feature_lists=tf.train.FeatureLists(feature_list={
                 'response': tfrecord_utils.int64_feature_list(response),
                 'prompt': tfrecord_utils.int64_feature_list(prompt)}))
@@ -113,21 +123,21 @@ def main(argv=None):
     filename = 'relevance.valid.tfrecords'
     print 'Writing', filename
     writer = tf.python_io.TFRecordWriter(filename)
-    for response, prompt, q_id, grd, spkr in zip(valid_responses, valid_prompts, valid_q_ids, valid_grades, valid_speakers):
+    for response, prompt, q_id, grd, spkr in zip(valid_responses, valid_prompts, valid_q_ids, valid_grades,
+                                                 valid_speakers):
         example = tf.train.SequenceExample(
             context=tf.train.Features(feature={
                 'targets': tfrecord_utils.float_feature([1.0]),
                 'grade': tfrecord_utils.float_feature([grd]),
                 'spkr': tfrecord_utils.bytes_feature([spkr]),
-                'q_id': tfrecord_utils.int64_feature([q_id])#,
-                #'section': tfrecord_utils.int64_feature(section),
-             }),
+                'q_id': tfrecord_utils.int64_feature([q_id])  # ,
+                # 'section': tfrecord_utils.int64_feature(section),
+            }),
             feature_lists=tf.train.FeatureLists(feature_list={
                 'response': tfrecord_utils.int64_feature_list(response),
                 'prompt': tfrecord_utils.int64_feature_list(prompt)}))
         writer.write(example.SerializeToString())
     writer.close()
-
 
 
 if __name__ == '__main__':
