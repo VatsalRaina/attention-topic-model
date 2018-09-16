15e7cff53bd227ab942aebe783f6ded5f05d2fac
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2018-09-16 20:31:37 +0100

    Add the flag option for saving reordered input

--- atm/run/step_test_atm.py
+++ atm/run/step_test_atm.py
@@ -29,6 +29,11 @@ commandLineParser.add_argument('--debug', type=int, default=0,
 commandLineParser.add_argument('--epoch', type=str, default=None,
                                help='which should be loaded')
 commandLineParser.add_argument('--wlist_path', type=str, default=None)
+commandLineParser.add_argument('--save_reordered_input', action='store_true', help="Whether to save the input words and"
+                                                                                   "responses to model as reordered during"
+                                                                                   "evaluation to the evaluation directory.")
+commandLineParser.add_argument('--make_plots', action='store_true',
+                               help="Whether to make the ROC and PR plots for the model.")
 commandLineParser.add_argument('data_pattern', type=str,
                                help='absolute path to response data')
 commandLineParser.add_argument('output_dir', type=str, default=None,
@@ -36,8 +41,10 @@ commandLineParser.add_argument('output_dir', type=str, default=None,
 
 
 
-def main(argv=None):
-    args = commandLineParser.parse_args()
+def main(args):
+    if args.save_reordered_input:
+        assert args.wlist_path != None
+
     if not os.path.isdir('CMDs'):
         os.mkdir('CMDs')
     with open('CMDs/step_test_attention_grader.cmd', 'a') as f:
@@ -63,7 +70,7 @@ def main(argv=None):
     test_response_lens_arr, \
     test_prompt_lens_arr, \
     test_responses_list, \
-    test_prompts_list = atm.predict((args.data_pattern))
+    test_prompts_list = atm.predict(args.data_pattern)
 
     # Save the numerical output data
     data = np.concatenate((test_labels_arr, test_probs_arr), axis=1)
@@ -73,28 +80,29 @@ def main(argv=None):
     np.savetxt(os.path.join(args.output_dir, 'response_lengths.txt'), test_response_lens_arr)
     np.savetxt(os.path.join(args.output_dir, 'prompt_lengths.txt'), test_prompt_lens_arr)
 
-    # Retrieve prompts and responses and convert to a list of lists
-    responses_word_ids = map(lambda i: list(test_responses_list[i][:test_response_lens_arr[i, 0]]),
-                             range(len(test_responses_list)))
-    prompts_word_ids = map(lambda i: list(test_prompts_list[i][:test_prompt_lens_arr[i, 0]]),
-                           range(len(test_prompts_list)))
-    # Save the prompts and responses
-    with open(os.path.join(args.output_dir, 'responses_word_ids.txt'), 'w') as response_file:
-        responses_str_lines = map(lambda x: ' '.join(x), responses_word_ids)
-        response_file.write('\n'.join(responses_str_lines))
-    with open(os.path.join(args.output_dir, 'prompts_word_ids.txt'), 'w') as prompt_file:
-        prompts_str_lines = map(lambda x: ' '.join(x), prompts_word_ids)
-        prompt_file.write('\n'.join(prompts_str_lines))
+    if args.save_reordered_input:
+        # Retrieve prompts and responses and convert to a list of lists
+        responses_word_ids = map(lambda i: list(test_responses_list[i][:test_response_lens_arr[i, 0]]),
+                                 range(len(test_responses_list)))
+        prompts_word_ids = map(lambda i: list(test_prompts_list[i][:test_prompt_lens_arr[i, 0]]),
+                               range(len(test_prompts_list)))
+        # Save the prompts and responses
+        with open(os.path.join(args.output_dir, 'responses_word_ids.txt'), 'w') as response_file:
+            responses_str_lines = map(lambda x: ' '.join(x), responses_word_ids)
+            response_file.write('\n'.join(responses_str_lines))
+        with open(os.path.join(args.output_dir, 'prompts_word_ids.txt'), 'w') as prompt_file:
+            prompts_str_lines = map(lambda x: ' '.join(x), prompts_word_ids)
+            prompt_file.write('\n'.join(prompts_str_lines))
 
-    # Process responses and prompts to get actual words
-    if args.wlist_path is not None:
-        id_converter = IdToWordConverter(args.wlist_path)
-        responses_words = map(lambda id_list: ' '.join(id_converter.id_list_to_word(id_list)), responses_word_ids)
-        prompts_words = map(lambda id_list: ' '.join(id_converter.id_list_to_word(id_list)), prompts_word_ids)
-        with open(os.path.join(args.output_dir, 'responses.txt'), 'w') as response_file:
-            response_file.write('\n'.join(responses_words))
-        with open(os.path.join(args.output_dir, 'prompts.txt'), 'w') as prompt_file:
-            prompt_file.write('\n'.join(prompts_words))
+        # Process responses and prompts to get actual words
+        if args.wlist_path is not None:
+            id_converter = IdToWordConverter(args.wlist_path)
+            responses_words = map(lambda id_list: ' '.join(id_converter.id_list_to_word(id_list)), responses_word_ids)
+            prompts_words = map(lambda id_list: ' '.join(id_converter.id_list_to_word(id_list)), prompts_word_ids)
+            with open(os.path.join(args.output_dir, 'responses.txt'), 'w') as response_file:
+                response_file.write('\n'.join(responses_words))
+            with open(os.path.join(args.output_dir, 'prompts.txt'), 'w') as prompt_file:
+                prompt_file.write('\n'.join(prompts_words))
 
     # Do evaluations, calculate metrics, etc...
     roc_score = roc(np.squeeze(test_labels_arr), np.squeeze(test_probs_arr))
@@ -102,35 +110,36 @@ def main(argv=None):
     test_labels_arr = np.squeeze(test_labels_arr)
     test_probs_arr = np.squeeze(test_probs_arr)
 
-    fpr, tpr, thresholds = roc_curve(np.asarray(test_labels_arr, dtype=np.int32), test_probs_arr)
-    plt.plot(fpr, tpr, c='r')
-    plt.plot([0, 1], [0, 1], 'k--', lw=4)
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.savefig(os.path.join(args.output_dir, 'test_roc_curve.png'))
-    plt.close()
+    if args.make_plots:
+        fpr, tpr, thresholds = roc_curve(np.asarray(test_labels_arr, dtype=np.int32), test_probs_arr)
+        plt.plot(fpr, tpr, c='r')
+        plt.plot([0, 1], [0, 1], 'k--', lw=4)
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.0])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.savefig(os.path.join(args.output_dir, 'test_roc_curve.png'))
+        plt.close()
 
-    precision, recall, thresholds = precision_recall_curve(test_labels_arr, test_probs_arr)
-    aupr_rel = auc(recall, precision)
-    plt.plot(recall, precision)
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.ylim(0.0, 1.0)
-    plt.xlim(0.0, 1.0)
-    plt.savefig(os.path.join(args.output_dir, 'test_pr_curve_relevant.png'))
-    plt.close()
+        precision, recall, thresholds = precision_recall_curve(test_labels_arr, test_probs_arr)
+        aupr_rel = auc(recall, precision)
+        plt.plot(recall, precision)
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.ylim(0.0, 1.0)
+        plt.xlim(0.0, 1.0)
+        plt.savefig(os.path.join(args.output_dir, 'test_pr_curve_relevant.png'))
+        plt.close()
 
-    precision, recall, thresholds = precision_recall_curve(1 - test_labels_arr, 1.0 - test_probs_arr)
-    aupr_nonrel = auc(recall, precision)
-    plt.plot(recall, precision)
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.ylim(0.0,1.0)
-    plt.xlim(0.0,1.0)
-    plt.savefig(os.path.join(args.output_dir, 'test_pr_curve_off-topic.png'))
-    plt.close()
+        precision, recall, thresholds = precision_recall_curve(1 - test_labels_arr, 1.0 - test_probs_arr)
+        aupr_nonrel = auc(recall, precision)
+        plt.plot(recall, precision)
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.ylim(0.0, 1.0)
+        plt.xlim(0.0, 1.0)
+        plt.savefig(os.path.join(args.output_dir, 'test_pr_curve_off-topic.png'))
+        plt.close()
 
     with open(os.path.join(args.output_dir, 'results.txt'), 'a') as f:
         f.write('Epoch: ' + str(args.epoch) + '\n')
@@ -140,4 +149,5 @@ def main(argv=None):
         f.write('Cross Entropy:' + str(np.round(test_loss, 3)) + '\n\n')
 
 if __name__ == '__main__':
-    main()
+    args = commandLineParser.parse_args()
+    main(args)
