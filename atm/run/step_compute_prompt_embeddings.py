cebf7de23b6bddf47d790e818fe17b3a169ca21d
am969 <am969@cam.ac.uk>
2018-10-18 12:06:27 +0100

    Merge branch 'development' of https://github.com/KaosEngineer/attention-topic-model
    Conflicts:
    Added HATM and new version evaluation script for it

--- atm/run/step_compute_prompt_embeddings.py
+++ atm/run/step_compute_prompt_embeddings.py
@@ -7,7 +7,7 @@ import sys
 
 import context
 from atm.atm import AttentionTopicModel
-from core.utilities.utilities import text_to_array
+from core.utilities.utilities import text_to_array, IdToWordConverter
 
 commandLineParser = argparse.ArgumentParser(description='Compute features from labels.')
 commandLineParser.add_argument('--load_path', type=str, default='./',
@@ -20,6 +20,7 @@ commandLineParser.add_argument('prompt_path', type=str,
                                help='which should be loaded')
 commandLineParser.add_argument('wlist_path', type=str,
                                help='which should be loaded')
+commandLineParser.add_argument('--strip_start_end', action='store_true', help='whether to strip the <s> </s> marks at the beginning and end of prompts in sorted_topics.txt file (used for legacy sorted_topics.txt formatting')
 
 
 def main(argv=None):
@@ -29,9 +30,12 @@ def main(argv=None):
     with open('CMDs/step_compute_prompt_embeddings.cmd', 'a') as f:
         f.write(' '.join(sys.argv) + '\n')
 
-    prompts, prompt_lens = text_to_array(args.prompt_path, input_index=args.wlist_path)
-    # Initialize and Run the Model
+    if args.strip_start_end:
+        print("Stripping the first and last word (should correspond to <s> and </s> marks) from the input prompts. Should only be used with legacy dataset formatting")
 
+
+    prompts, prompt_lens = text_to_array(args.topic_path, args.wlist_path, strip_start_end=args.strip_start_end)
+    # Initialize and Run the Model
     atm = AttentionTopicModel(network_architecture=None,
                               load_path=args.load_path,
                               debug_mode=args.debug,
