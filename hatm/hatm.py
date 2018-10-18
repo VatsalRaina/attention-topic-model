ff2fdca543aa0022960acf9b825b6dc8e550cb99
am969 <am969@cam.ac.uk>
2018-10-18 13:46:27 +0100

    Merge branch 'development' of https://github.com/KaosEngineer/attention-topic-model
    Conflicts:
    Added HATM and new version evaluation script for it

--- hatm/hatm.py
+++ hatm/hatm.py
@@ -325,6 +325,8 @@ class HierarchicialAttentionTopicModel(BaseModel):
             variables1 =tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='.*((PROMPT_ATN)|(RNN_KEY)).*')
             variables2 = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
                                            scope='.*((PROMPT_ATN)|(RNN_KEY)|(Grader_Attention)).*')
+            for val in variables2:
+                print val.name
             train_op_new = util.create_train_op(total_loss=total_loss,
                                             learning_rate=learning_rate,
                                             optimizer=optimizer,
