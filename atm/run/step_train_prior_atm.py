0a749fdde5c4d5f145c3eaa5057049a0138b197c
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2018-10-25 15:51:39 +0100

    Redesign CML interface to be more usable

--- atm/run/step_train_prior_atm.py
+++ atm/run/step_train_prior_atm.py
@@ -11,6 +11,23 @@ from core.utilities.utilities import text_to_array, get_train_size_from_meta
 from atm.atm import ATMPriorNetwork
 
 parser = argparse.ArgumentParser(description='Compute features from labels.')
+parser.add_argument('data_dir_in_domain', type=str,
+                    help='Directory with in domain data.')
+parser.add_argument('data_dir_out_domain', type=str,
+                    help='Directory with out of domain data.')
+
+parser.add_argument('--valid_file', type=str, default='relevance.valid.tfrecords',
+                    help='file name of the validation tfrecords file')
+parser.add_argument('--train_file', type=str, default='relevance.train.tfrecords',
+                    help='file name of the training tfrecords file')
+parser.add_argument('--topic_file', type=str, default='sorted_topics.txt',
+                    help='which should be loaded')
+parser.add_argument('--topic_count_file', type=str, default='unigrams.txt',
+                    help='which should be loaded')
+parser.add_argument('--wlist_file', type=str, default='input.wlist.index',
+                    help='which should be loaded')
+parser.add_argument('--meta_file', type=str, default='dataset_meta.txt')
+
 parser.add_argument('--valid_size', type=int, default=14188,
                                help='Specify the validation set size')
 parser.add_argument('--batch_size', type=int, default=100,
@@ -39,26 +56,6 @@ parser.add_argument('--distortion', type=float, default=1.0,
                                help='Specify whether to use uniform negative sampliong')
 parser.add_argument('--epoch', type=str, default=None,
                                help='which should be loaded')
-parser.add_argument('train_data_in_domain', type=str,
-                    help='which should be loaded')
-parser.add_argument('train_data_out_domain', type=str,
-                    help='which should be loaded')
-parser.add_argument('meta_data_path_in_domain', type=str,
-                    help='Path to the meta data file (which contains the dataset size and number of topics).')
-parser.add_argument('meta_data_path_out_domain', type=str,
-                    help='Path to the meta data file (which contains the dataset size and number of topics).')
-parser.add_argument('valid_data', type=str,
-                               help='which should be loaded')
-parser.add_argument('topic_path', type=str,
-                               help='which should be loaded')
-parser.add_argument('topic_count_path', type=str,
-                               help='which should be loaded')
-parser.add_argument('topic_path_in_domain', type=str,
-                    help='which should be loaded')
-parser.add_argument('topic_count_path_out_domain', type=str,
-                    help='which should be loaded')
-parser.add_argument('wlist_path', type=str,
-                               help='which should be loaded')
 parser.add_argument('--strip_start_end', action='store_true', help='whether to strip the <s> </s> marks at the beginning and end of prompts in sorted_topics.txt file (used for legacy sorted_topics.txt formatting')
 
 
@@ -69,12 +66,34 @@ def main(args):
         f.write(' '.join(sys.argv) + '\n')
         f.write('--------------------------------\n')
 
-    train_size = get_train_size_from_meta(args.meta_data_path_in_domain)
+    tfrecords_dir_in_domain = os.path.join(args.data_dir_in_domain, 'tfrecords')
+    tfrecords_dir_out_domain = os.path.join(args.data_dir_out_domain, 'tfrecords')
 
-    topics_in_domain, topic_lens_in_domain = text_to_array(args.topic_path_out_domain, args.wlist_path,
+    topic_path_in_domain = os.path.join(tfrecords_dir_in_domain, args.topic_file)
+    topic_path_out_domain = os.path.join(tfrecords_dir_out_domain, args.topic_file)
+
+    wlist_path = os.path.join(tfrecords_dir_in_domain, args.wlist_file)
+
+    topic_count_path_in_domain = os.path.join(tfrecords_dir_in_domain, args.topic_count_file)
+    topic_count_path_out_domain = os.path.join(tfrecords_dir_out_domain, args.topic_count_file)
+
+    train_data_in_domain = os.path.join(tfrecords_dir_in_domain, args.train_file)
+    train_data_out_domain = os.path.join(tfrecords_dir_out_domain, args.train_file)
+
+    valid_data = os.path.join(tfrecords_dir_in_domain, args.valid_file)
+
+    dataset_meta_path_in_domain = os.path.join(tfrecords_dir_in_domain, args.meta_file)
+    dataset_meta_path_out_domain = os.path.join(tfrecords_dir_out_domain, args.meta_file)
+
+    train_size = get_train_size_from_meta(dataset_meta_path_in_domain)
+    # train_size_out_domain = get_train_size_from_meta(dataset_meta_path_out_domain)
+
+    topics_in_domain, topic_lens_in_domain = text_to_array(topic_path_in_domain, wlist_path,
                                                            strip_start_end=args.strip_start_end)
-    topics_out_domain, topic_lens_out_domain = text_to_array(args.topic_path_out_domain, args.wlist_path,
+    topics_out_domain, topic_lens_out_domain = text_to_array(topic_path_out_domain, wlist_path,
                                                              strip_start_end=args.strip_start_end)
+
+
     if args.strip_start_end: print("Stripping the first and last word (should correspond to <s> and </s> marks) "
                                    "from the input prompts. Should only be used with legacy dataset formatting")
 
@@ -86,16 +105,16 @@ def main(args):
                           debug_mode=args.debug,
                           epoch=args.epoch)
 
-    atm.fit_prior_net(train_data_in_domain=args.train_data_in_domain,
-                      train_data_out_domain=args.train_data_out_domain,
-                      valid_data=args.valid_data,
+    atm.fit_prior_net(train_data_in_domain=train_data_in_domain,
+                      train_data_out_domain=train_data_out_domain,
+                      valid_data=valid_data,
                       load_path=args.init,
                       topics_in_domain=topics_in_domain,
                       topics_out_domain=topics_out_domain,
                       topic_lens_in_domain=topic_lens_in_domain,
                       topic_lens_out_domain=topic_lens_out_domain,
-                      unigram_path_in_domain=args.topic_count_path_in_domain,
-                      unigram_path_out_domain=args.topic_count_path_out_domain,
+                      unigram_path_in_domain=topic_count_path_in_domain,
+                      unigram_path_out_domain=topic_count_path_out_domain,
                       train_size=train_size,
                       learning_rate=args.learning_rate,
                       lr_decay=args.lr_decay,
@@ -107,8 +126,6 @@ def main(args):
                       n_epochs=args.n_epochs,
                       epoch=0,
                       which_trn_cost='contrastive')
-
-
     atm.save()
 
 if __name__ == '__main__':
